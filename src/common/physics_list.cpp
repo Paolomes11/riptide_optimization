481835aa2c27a20d6e86c4f3f63f522f6ffeffdc
Paolomes11 <200260paolo@gmail.com>
2026-03-17 18:37:55 +0100

    Better memory and speed (150Mb, 16s)

--- src/common/physics_list.cpp
+++ src/common/physics_list.cpp
@@ -14,13 +14,9 @@
 
 #include "physics_list.hpp"
 
-#include <G4DecayPhysics.hh>
 #include <G4EmStandardPhysics.hh>
-#include <G4HadronElasticPhysics.hh>
-#include <G4HadronPhysicsINCLXX.hh>
 #include <G4OpticalParameters.hh>
 #include <G4OpticalPhysics.hh>
-#include <G4StoppingPhysics.hh>
 #include <G4SystemOfUnits.hh>
 
 namespace riptide {
@@ -29,11 +25,15 @@ PhysicsList::PhysicsList() {
   G4VModularPhysicsList::defaultCutValue = 1.0 * mm;
 
   RegisterPhysics(new G4EmStandardPhysics{0});
-  RegisterPhysics(new G4HadronElasticPhysics{0});
-  RegisterPhysics(new G4HadronPhysicsINCLXX("hadronPhysicsINCLXX"));
-  RegisterPhysics(new G4DecayPhysics{0});
-  RegisterPhysics(new G4StoppingPhysics{0});
+
   RegisterPhysics(new G4OpticalPhysics{0});
+
+  // Disabilita processi ottici non necessari (fotoni generati via GPS)
+  auto* optParams = G4OpticalParameters::Instance();
+  optParams->SetProcessActivation("Scintillation", false);
+  optParams->SetProcessActivation("Cerenkov", false);
+  optParams->SetProcessActivation("WLS", false);
+  optParams->SetProcessActivation("WLS2", false);
 }
 
 } // namespace riptide
